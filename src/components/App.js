fd512d06463d79b0b5f7ed32ec560860458b8450
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-07 17:40:54 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import { AuthContext } from "./Auththenticate";
+import ChekBox from "./ChekBox";
 const App = () => {
+  const [auth,setAuth]= useState(false);
+  const authText= "You are now authenticated, you can proceed"
+  const noAuth="you are not authenticated"
   return (
+    <AuthContext.Provider value={{auth,setAuth,authText,noAuth}}>
+
     <div>
-        {/* Do not remove the main div */}
+
+           <h1>Click on the checkbox to get authenticaed</h1>
+           <ChekBox/>     
     </div>
+    </AuthContext.Provider>
   )
 }
 
---
+++ src/components/ChekBox.js
@@ -0,0 +1,22 @@
+// import react from react
+import React from "react";
+import { useContext } from "react"
+import { AuthContext } from "./Auththenticate"
+
+
+const ChekBox=()=>{
+    const data= useContext(AuthContext);
+    const {auth,setAuth,authText,noAuth} = data;
+    
+    const handleCheck=(e)=>{
+     console.log(e.target.checked);
+     setAuth(e.target.checked)
+    }
+    return <div>
+        {auth ? <p>{authText}</p>: <p>{noAuth}</p>}
+        <input type="checkbox" onChange={handleCheck}/>
+        <p  style={{display:"inline"}}>I'am not a robot</p>
+    </div>
+}
+
+export default ChekBox
